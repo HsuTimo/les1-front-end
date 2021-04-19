9e3d0c02125920c1b5f0bdc6e8f874bc482e7b97
HsuTimo <72869492+HsuTimo@users.noreply.github.com>
2021-04-19 13:16:33 +0200

    testing javascript

---
+++ testscript.js
@@ -0,0 +1,7 @@
+console.log('Hello world');
+let a = 20;
+let b = 16;
+let difference = a - b;
+console.log('difference between '+a+' and '+b+' is '+difference);
+a = true;
+console.log("what happend to a? what is it's value now? it is: "+ a);
